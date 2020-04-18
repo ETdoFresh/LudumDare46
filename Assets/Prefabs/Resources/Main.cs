e3a6dc96d1550edfb1b12a4bbccf6614158764b6
ETdoFresh <ETdoFresh@gmail.com>
2020-04-18 15:39:48 -0500

    Import TextMesh Pro. Create Scenes. Simple Menu Scene and EnterSpaceshipScene started. Create Main Prefab for Global Objects.

---
+++ Assets/Prefabs/Resources/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public static Main singleton;
+    public DialogWindow dialogWindow;
+
+    [RuntimeInitializeOnLoadMethod]
+    public static void CreateMainIfNotExists()
+    {
+        if (singleton) return;
+
+        var mainPrefab = Resources.Load<GameObject>("Main");
+        singleton = Instantiate(mainPrefab).GetComponent<Main>();
+        singleton.name = "Main";
+        DontDestroyOnLoad(singleton.gameObject);
+    }
+    
+    public static DialogWindow GetDialogWindow()
+    {
+        if (!singleton) return null;
+        return singleton.dialogWindow;
+    }
+}

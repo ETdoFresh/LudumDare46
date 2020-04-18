8fdbd68a9779af299acf957a02b52aacf5684eaa
ETdoFresh <ETdoFresh@gmail.com>
2020-04-17 21:10:44 -0500

    Initial Commit.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float movementForce;
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        rigidbody2D.MovePosition(transform.position + (input * movementForce));
+    }
+}
